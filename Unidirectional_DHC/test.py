67fcc3fb21e9616dbea1a325e22d8717898e954d
LKivi <43588272+LKivi@users.noreply.github.com>
2018-10-11 16:07:53 +0200

    Energy System Optimization

---
+++ Unidirectional_DHC/test.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 10 22:46:17 2018
+
+@author: lkivi
+"""
+import cmath
+import numpy as np
+
+dic = {"a": 5,
+       "b": 1}
+
+dic2 = {"c":2}
+
+
+dic.update(dic2)
+
+print(dic)
+
+a = 1+5j
+b = 3+4j
+
+print()
